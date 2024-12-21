8bfbc7faa0cb8f6cf5ed1e350c74c92fa8d8e576
Zandgaim <1zandgaim@gmail.com>
2024-12-21 17:59:45 +0100

    python_port rewright

--- lib/scada/python_port.ex
+++ lib/scada/python_port.ex
@@ -16,27 +16,32 @@ defmodule Scada.PythonPort do
   def init(:ok) do
     state = %{
       connected: false,
+      port: nil,
       status: "waiting",
       message: "Not connected to machine",
       data: nil
     }
 
-    check_connection()
+    connect()
     {:ok, state}
   end
 
-  def handle_info(:check_connection, state) do
-    GenServer.cast(__MODULE__, :connect)
-    check_connection()
+  def handle_info(:connect, %{connected: true} = state) do
     {:noreply, state}
   end
 
-  def handle_info({_port, {:data, data}}, state) do
+  def handle_info(:connect, %{connected: false} = state) do
+    GenServer.cast(__MODULE__, :connect_to_ads)
+    connect()
+    {:noreply, state}
+  end
+
+  def handle_info({port, {:data, data}}, state) do
     IO.puts("Received response = #{inspect(Jason.decode(data))}")
 
     case Jason.decode(data) do
       {:ok, %{"routing_key" => routing_key} = response} ->
-        handle_routing_key(routing_key, response, state)
+        handle_routing_key(port, routing_key, response, state)
 
       {:error, _reason} ->
         new_state = %{
@@ -50,13 +55,20 @@ defmodule Scada.PythonPort do
     end
   end
 
-  def handle_cast(:connect, state) do
-    %{
-      command: "connect"
-    }
-    |> send_to_ads()
+  def handle_info({:EXIT, port, reason}, state) do
+    IO.puts("Received exit signal from port: #{inspect(port)} with reason: #{inspect(reason)}")
 
     {:noreply, state}
+  end
+
+  def handle_cast(:connect_to_ads, state) do
+    port =
+      %{command: "connect"}
+      |> connect_to_ads()
+
+    IO.puts("ACHTUNG port = #{inspect(port)}")
+
+    {:noreply, %{state | port: port}}
   end
 
   def handle_call({:fetch_data, data}, _from, state) do
@@ -79,6 +91,25 @@ defmodule Scada.PythonPort do
     GenServer.call(__MODULE__, {:fetch_data, fields})
   end
 
+  defp connect_to_ads(request) do
+    request =
+      %{
+        ams_net_id: @ams_net_id,
+        ams_port: @ams_port
+      }
+      |> Map.merge(request)
+      |> Jason.encode!()
+
+    command = [
+      @ads_service,
+      "--loop",
+      "--request",
+      request
+    ]
+
+    Port.open({:spawn_executable, @python_env}, [:binary, args: command])
+  end
+
   defp send_to_ads(request) do
     request =
       %{
@@ -97,10 +128,11 @@ defmodule Scada.PythonPort do
     Port.open({:spawn_executable, @python_env}, [:binary, args: command])
   end
 
-  defp handle_routing_key("connect", %{"status" => "connected", "message" => message}, state) do
+  defp handle_routing_key(port, "connect", %{"status" => "connected", "message" => message}, state) do
     new_state = %{
       state
-      | connected: true,
+      | port: port,
+        connected: true,
         status: "connected",
         message: message
     }
@@ -109,10 +141,11 @@ defmodule Scada.PythonPort do
     {:noreply, new_state}
   end
 
-  defp handle_routing_key("connect", %{"status" => "error", "message" => error_message}, state) do
+  defp handle_routing_key(_, "connect", %{"status" => "error", "message" => error_message}, state) do
     new_state = %{
       state
-      | connected: false,
+      | port: nil,
+        connected: false,
         status: "error",
         message: error_message
     }
@@ -121,21 +154,21 @@ defmodule Scada.PythonPort do
     {:noreply, new_state}
   end
 
-  defp handle_routing_key("fetch_data", %{"message" => message, "data" => data}, state) do
+  defp handle_routing_key(_, "fetch_data", %{"message" => message, "data" => data}, state) do
     new_state = %{state | message: message, data: data}
 
     broadcast(new_state)
     {:noreply, new_state}
   end
 
-  defp handle_routing_key("fetch_data", %{"message" => message}, state) do
+  defp handle_routing_key(_, "fetch_data", %{"message" => message}, state) do
     new_state = %{state | message: message, data: nil}
 
     broadcast(new_state)
     {:noreply, new_state}
   end
 
-  defp handle_routing_key(_unknown_key, %{"message" => message}, state) do
+  defp handle_routing_key(_, _unknown_key, %{"message" => message}, state) do
     new_state = %{
       state
       | status: "error",
@@ -146,8 +179,8 @@ defmodule Scada.PythonPort do
     {:noreply, new_state}
   end
 
-  defp check_connection do
-    Process.send_after(self(), :check_connection, @retry_interval)
+  defp connect do
+    Process.send_after(self(), :connect, @retry_interval)
   end
 
   defp broadcast(state) do
